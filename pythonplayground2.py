786f125d733ec8077bf2d30e63613a83073b50a3
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-07-30 01:18:15 +0500

    modules

---
+++ pythonplayground2.py
@@ -0,0 +1,18 @@
+#decorator
+def div(a,b):
+    print(a/b)
+
+def smart_div(func):
+    def inner(a,b):
+        if a < b :
+            a,b = b,a
+        return func(a,b)
+    return inner
+div = smart_div(div)
+div(2,4)
+
+
+#Modules
+import pythonplayground
+
+pythonplayground.factRe(5)
