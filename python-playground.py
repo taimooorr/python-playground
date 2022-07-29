e770a23dbb1dfcb5b0b051ffedd12337885cd76d
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-07-30 00:32:30 +0500

    passing list to function

--- python-playground.py
+++ python-playground.py
@@ -14,3 +14,19 @@ def tryGlobals() :
     globals()['a'] =40
     print(b)
 tryGlobals()
+
+# passing list to Fuction
+def passList(lst) :
+    odd = 0
+    even = 0
+    for i in lst :
+        if i % 2 == 0 :
+            even += 1
+        else :
+            odd += 1
+    return odd, even
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+
+even,odd = passList(lst)
+print("Even : {} and Odd : {}".format(even,odd))
