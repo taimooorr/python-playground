b44ef6ac3b2dd77c4e012f3c3e35263cf44dfd44
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-08-10 17:00:24 +0500

    Accessor and Mutators

--- playground3.py
+++ playground3.py
@@ -1,2 +1,25 @@
-class person:
-    #
+class Person :
+
+    def __init__(self,Name,Age):
+        self.name = Name
+        self.age = Age
+        
+        #Accessor and Mutator methods in Python
+    def getName(self):
+        return self.name
+    def setName(self,Name):
+        self.name = Name
+    def getAge(self):
+        return self.age
+
+    def display(self):
+        print("Name: ",self.name)
+        print("Age: ",self.age)
+    
+    
+Person1 = Person("John", 23)
+Person1.display()
+Person1.setName("Jack")
+Person1.display()
+
+
